13b2f134fd2046d0dadf955107ecfc79d044b918
Tim Hegeman <tim.m.hegeman@gmail.com>
2019-01-10 11:10:29 +0100

    Add deployment of Spark in standalone mode

--- big_data_deployer/__init__.py
+++ big_data_deployer/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
 
 from frameworkmanager import Framework, FrameworkVersion, FrameworkRegistry, FrameworkManager, get_framework_registry
-import hadoop, zookeeper
+import hadoop, zookeeper, spark
 
